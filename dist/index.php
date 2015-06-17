65b481f7c8aebaa073d7708653cccdddff503c77
userx <sgoodman@aiddata.org>
2015-06-17 15:17:44 -0400

    Add no cache headers.

--- dist/index.php
+++ dist/index.php
@@ -2,6 +2,13 @@
 <html>
   <head>
     <meta charset="UTF-8">
+    
+    <?php
+        header('Cache-Control: no-cache, no-store, must-revalidate'); // HTTP 1.1.
+        header('Pragma: no-cache'); // HTTP 1.0.
+        header('Expires: 0'); // Proxies.
+    ?>
+
     <title></title>
     <link rel="icon" type="image/png" href="">
 
